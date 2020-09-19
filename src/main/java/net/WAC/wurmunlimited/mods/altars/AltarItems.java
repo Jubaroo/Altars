acb2ab7cd8cbd8a1185da6c3722e6946d1dce814
Zeus <requiemofwurm@gmail.com>
2020-09-19 16:41:07 -0400

    Minor Internal Changes

--- src/main/java/net/WAC/wurmunlimited/mods/altars/AltarItems.java
+++ src/main/java/net/WAC/wurmunlimited/mods/altars/AltarItems.java
@@ -6,41 +6,92 @@ import com.wurmonline.server.behaviours.BehaviourList;
 import com.wurmonline.server.items.*;
 import com.wurmonline.server.skills.SkillList;
 import com.wurmonline.shared.constants.IconConstants;
+import com.wurmonline.shared.constants.ItemMaterials;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
+import org.gotti.wurmunlimited.modsupport.ItemTemplateBuilder;
 
 import java.io.IOException;
 
 public class AltarItems implements WurmServerMod, ItemTypes, MiscConstants {
 
-    public static int hugeMarbleAltarTemplateId = 9918;
     public static float marbleAltarSizeMultiplier = 1f;
-    public static int sacrificialAltarTemplateId = 9919;
     public static float sacrificialAltarSizeMultiplier = 1f;
+    public static int HUGE_MARBLE_ALTAR_ID, SACRIFICIAL_ALTAR;
 
-    public AltarItems() {
-        try {
-            ItemTemplateCreator.createItemTemplate(hugeMarbleAltarTemplateId, ItemSizes.ITEM_SIZE_HUGE, "Altar", "Altars", "almost full", "somewhat occupied", "half-full", "emptyish",
-                    "A huge marble altar with beautiful hand carved designs all around it.",
-                    new short[]{
-                            ITEM_TYPE_NAMED,
-                            ITEM_TYPE_HOLLOW,
-                            ITEM_TYPE_NOTAKE,
-                            ITEM_TYPE_STONE,
-                            ITEM_TYPE_TURNABLE,
-                            ITEM_TYPE_DECORATION,
-                            ITEM_TYPE_REPAIRABLE,
-                            ITEM_TYPE_HASDATA,
-                            ITEM_TYPE_DOMAIN,
-                            ITEM_TYPE_USE_GROUND_ONLY,
-                            ITEM_TYPE_OWNER_DESTROYABLE,
-                            ITEM_TYPE_HOLLOW_VIEWABLE
-                    },
-                    (short) IconConstants.ICON_NONE, BehaviourList.domainItemBehaviour, 0, TimeConstants.DECAYTIME_STONE, 1000, 1000, 1000, (int) MiscConstants.NOID,
-                    MiscConstants.EMPTY_BYTE_PRIMITIVE_ARRAY, "model.decoration.altar.huge.marble.", 50.0F, 460000, Materials.MATERIAL_MARBLE, 5000, false).setContainerSize((int) (500 * marbleAltarSizeMultiplier), (int) (500 * marbleAltarSizeMultiplier), (int) (500 * marbleAltarSizeMultiplier));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        AdvancedCreationEntry altarHuge = CreationEntryCreator.createAdvancedEntry(SkillList.MASONRY, ItemList.marbleSlab, ItemList.marbleBrick, hugeMarbleAltarTemplateId, false, false, 0.0f, true, true, CreationCategories.ALTAR);
+    public static void registerItems() throws IOException {
+        registerHugeAltar();
+        registerSacrificialAltar();
+    }
+
+    public static void registerHugeAltar() throws IOException {
+        ItemTemplate temp = new ItemTemplateBuilder("WAC.structure.altar.marble")
+                .name("Altar", "Altars", "A huge marble altar with beautiful hand carved designs all around it.")
+                .descriptions("almost full", "somewhat occupied", "half-full", "emptyish")
+                .modelName("model.decoration.altar.huge.marble.")
+                .imageNumber((short) IconConstants.ICON_NONE)
+                .weightGrams(460000)
+                .value(5000)
+                .size(ItemSizes.ITEM_SIZE_HUGE)
+                .difficulty(50.0F)
+                .dimensions(1000, 1000, 1000)
+                .decayTime(TimeConstants.DECAYTIME_STONE)
+                .material(ItemMaterials.MATERIAL_MARBLE)
+                .behaviourType(BehaviourList.domainItemBehaviour)
+                .itemTypes(new short[]{
+                        ITEM_TYPE_NAMED,
+                        ITEM_TYPE_HOLLOW,
+                        ITEM_TYPE_NOTAKE,
+                        ITEM_TYPE_STONE,
+                        ITEM_TYPE_TURNABLE,
+                        ITEM_TYPE_DECORATION,
+                        ITEM_TYPE_REPAIRABLE,
+                        ITEM_TYPE_HASDATA,
+                        ITEM_TYPE_DOMAIN,
+                        ITEM_TYPE_USE_GROUND_ONLY,
+                        ITEM_TYPE_OWNER_DESTROYABLE,
+                        ITEM_TYPE_HOLLOW_VIEWABLE
+                })
+                .containerSize((int) (500 * marbleAltarSizeMultiplier), (int) (500 * marbleAltarSizeMultiplier), (int) (500 * marbleAltarSizeMultiplier))
+                .build();
+
+        HUGE_MARBLE_ALTAR_ID = temp.getTemplateId();
+    }
+
+    public static void registerSacrificialAltar() throws IOException {
+        ItemTemplate temp = new ItemTemplateBuilder("WAC.structure.altar.sacrificial")
+                .name("Sacrificial Altar", "Sacrificial Altar", "A stone altar with foreign markings and dark energy. It is mainly used by cultists for rituals and sacrificing.")
+                .descriptions("almost full", "somewhat occupied", "half-full", "emptyish")
+                .modelName("model.decoration.altar.sacrificial.")
+                .imageNumber((short) IconConstants.ICON_NONE)
+                .weightGrams(290000)
+                .value(3000)
+                .difficulty(35.0F)
+                .dimensions(1000, 1000, 1000)
+                .decayTime(TimeConstants.DECAYTIME_STONE)
+                .material(ItemMaterials.MATERIAL_STONE)
+                .behaviourType(BehaviourList.domainItemBehaviour)
+                .itemTypes(new short[]{
+                        ITEM_TYPE_NAMED,
+                        ITEM_TYPE_HOLLOW,
+                        ITEM_TYPE_NOTAKE,
+                        ITEM_TYPE_STONE,
+                        ITEM_TYPE_TURNABLE,
+                        ITEM_TYPE_DECORATION,
+                        ITEM_TYPE_REPAIRABLE,
+                        ITEM_TYPE_HASDATA,
+                        ITEM_TYPE_DOMAIN,
+                        ITEM_TYPE_USE_GROUND_ONLY,
+                        ITEM_TYPE_OWNER_DESTROYABLE,
+                        ITEM_TYPE_HOLLOW_VIEWABLE
+                })
+                .containerSize((int) (250 * sacrificialAltarSizeMultiplier), (int) (250 * sacrificialAltarSizeMultiplier), (int) (250 * sacrificialAltarSizeMultiplier))
+                .build();
+
+        SACRIFICIAL_ALTAR = temp.getTemplateId();
+    }
+
+    public static void initCreationEntry() {
+        final AdvancedCreationEntry altarHuge = CreationEntryCreator.createAdvancedEntry(SkillList.MASONRY, ItemList.marbleSlab, ItemList.marbleBrick, HUGE_MARBLE_ALTAR_ID, false, false, 0.0f, true, true, CreationCategories.ALTAR);
         altarHuge.addRequirement(new CreationRequirement(1, ItemList.marbleSlab, 5, true));
         altarHuge.addRequirement(new CreationRequirement(2, ItemList.marbleBrick, 19, true));
         altarHuge.addRequirement(new CreationRequirement(3, ItemList.mortar, 10, true));
@@ -48,32 +99,7 @@ public class AltarItems implements WurmServerMod, ItemTypes, MiscConstants {
         altarHuge.addRequirement(new CreationRequirement(5, ItemList.cochineal, 5, true));
         altarHuge.addRequirement(new CreationRequirement(6, ItemList.plank, 4, true));
         altarHuge.addRequirement(new CreationRequirement(7, ItemList.sandstoneBrick, 10, true));
-
-
-
-        try {
-            ItemTemplateCreator.createItemTemplate(sacrificialAltarTemplateId, "Sacrificial Altar", "Sacrificial Altars", "almost full", "somewhat occupied", "half-full", "emptyish",
-                    "A stone altar with foreign markings and dark energy. It is mainly used by cultists for rituals and sacrificing.",
-                    new short[]{
-                            ITEM_TYPE_NAMED,
-                            ITEM_TYPE_HOLLOW,
-                            ITEM_TYPE_NOTAKE,
-                            ITEM_TYPE_STONE,
-                            ITEM_TYPE_TURNABLE,
-                            ITEM_TYPE_DECORATION,
-                            ITEM_TYPE_REPAIRABLE,
-                            ITEM_TYPE_HASDATA,
-                            ITEM_TYPE_DOMAIN,
-                            ITEM_TYPE_USE_GROUND_ONLY,
-                            ITEM_TYPE_OWNER_DESTROYABLE,
-                            ITEM_TYPE_HOLLOW_VIEWABLE
-                    },
-                    (short) IconConstants.ICON_NONE, BehaviourList.domainItemBehaviour, 0, TimeConstants.DECAYTIME_STONE, 1000, 1000, 1000, (int) MiscConstants.NOID,
-                    MiscConstants.EMPTY_BYTE_PRIMITIVE_ARRAY, "model.decoration.altar.sacrificial.", 35.0F, 290000, Materials.MATERIAL_STONE, 1000, false).setContainerSize((int) (250 * sacrificialAltarSizeMultiplier), (int) (250 * sacrificialAltarSizeMultiplier), (int) (250 * sacrificialAltarSizeMultiplier));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        AdvancedCreationEntry altarSacrificial = CreationEntryCreator.createAdvancedEntry(SkillList.MASONRY, ItemList.stoneSlab, ItemList.stoneBrick, sacrificialAltarTemplateId, false, false, 0.0f, true, true, CreationCategories.ALTAR);
+        final AdvancedCreationEntry altarSacrificial = CreationEntryCreator.createAdvancedEntry(SkillList.MASONRY, ItemList.stoneSlab, ItemList.stoneBrick, SACRIFICIAL_ALTAR, false, false, 0.0f, true, true, CreationCategories.ALTAR);
         altarSacrificial.addRequirement(new CreationRequirement(1, ItemList.stoneSlab, 4, true));
         altarSacrificial.addRequirement(new CreationRequirement(2, ItemList.stoneBrick, 9, true));
         altarSacrificial.addRequirement(new CreationRequirement(3, ItemList.mortar, 6, true));
--- src/main/java/net/WAC/wurmunlimited/mods/altars/Altars.java
+++ src/main/java/net/WAC/wurmunlimited/mods/altars/Altars.java
@@ -1,65 +1,32 @@
 package net.WAC.wurmunlimited.mods.altars;
 
-import com.wurmonline.server.items.ItemList;
-import com.wurmonline.server.items.ItemTemplate;
 import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
 import org.gotti.wurmunlimited.modloader.interfaces.ItemTemplatesCreatedListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Altars implements WurmServerMod, Configurable, ItemTemplatesCreatedListener {
 
-    public static final Logger logger = Logger.getLogger(Altars.class.getName());
-
     public String getVersion() {
-        return "v2.0";
+        return "v3.0";
     }
 
     @Override
     public void configure(Properties properties) {
-        AltarItems.hugeMarbleAltarTemplateId = Integer.parseInt(properties.getProperty("Huge_Marble_Altar_templateId", String.valueOf(AltarItems.hugeMarbleAltarTemplateId)));
         AltarItems.marbleAltarSizeMultiplier = Float.parseFloat(properties.getProperty("Huge_Marble_Altar_Size_Multiplier", String.valueOf(AltarItems.marbleAltarSizeMultiplier)));
-        AltarItems.sacrificialAltarTemplateId = Integer.parseInt(properties.getProperty("Sacrificial_Altar_templateId", String.valueOf(AltarItems.sacrificialAltarTemplateId)));
         AltarItems.sacrificialAltarSizeMultiplier = Float.parseFloat(properties.getProperty("Sacrificial_Altar_Size_Multiplier", String.valueOf(AltarItems.sacrificialAltarSizeMultiplier)));
     }
 
     @Override
     public void onItemTemplatesCreated() {
-        new AltarItems();
-        decorationItem(ItemList.temple, false);
-    }
-
-    public static Method _getItemTemplate = TweakApiPerms.getClassMeth(
-            "com.wurmonline.server.items.CreationWindowMethods",
-            "getItemTemplate",
-            "int");
-
-    public static ItemTemplate getItemTemplate(int id) {
-        if (_getItemTemplate == null) {
-            return null;
-        }
         try {
-            return (ItemTemplate) _getItemTemplate.invoke(null, id);
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            logger.log(Level.SEVERE, "getItemTemplate: " + e.toString());
-            return null;
+            AltarItems.registerItems();
+            AltarItems.initCreationEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    public static boolean decorationItem(int tempid, boolean b) {
-        if (_getItemTemplate == null) {
-            return true;
-        }
-        ItemTemplate tplat = getItemTemplate(tempid);
-        if (tplat == null) {
-            return true;
-        }
-        return TweakApiPerms.setItemField(tplat, "decoration", b);
     }
 
 }
